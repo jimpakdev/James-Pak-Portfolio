023c4926d9ef940c59e4216cc42ae1f794f460b6
jimpakdev <jamesjiminpak@gmail.com>
2020-01-10 20:48:21 -0800

    fontawesome icons added

--- portfolio/src/Components/AboutMe.js
+++ portfolio/src/Components/AboutMe.js
@@ -11,7 +11,7 @@ class AboutMe extends Component {
 
         return (
             <>
-                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-start", alignItems: "center", marginTop: "180px" }}>
+                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-start", alignItems: "center", marginTop: "180px", width: "auto" }}>
 
                         <div style={{ display: "flex", flexDirection: "column", marginLeft: "100px", marginRight: "30px",  }}>
                             <Link to="/" style={{margin: "10px", marginBottom: "20px", fontSize: "20px", textDecoration: "none"}}>About Me</Link>
@@ -22,12 +22,24 @@ class AboutMe extends Component {
                         <img src={profile} alt="Profile Pic" style={{marginLeft: "30px", marginRight: "30px", width: "100px", height: "100px", borderRadius: "50%" }}/>
 
 
-                        <div style={{marginLeft: "60px", maxWidth: "700px", fontSize: "30px", fontFamily: "Times New Roman", lineHeight: "40px"}}>
+                        <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start", marginTop: "20px", marginLeft: "60px", maxWidth: "700px", fontSize: "30px", fontFamily: "Times New Roman", lineHeight: "40px"}}>
                             Hi!  I'm Jim.  I'm a full stack developer with a passion for building.  
                             I come from a non traditional tech background as I was previously a project manager in the construction industry.
                             I began by teaching myself HTML, CSS, and Javascript at night.  Currently I am developing applications in React and Node JS.
+                            <div style={{ marginTop: "20px" }}>
+                                <a href="#" style={{ fontSize: "50px", marginRight: "10px"}}><i class="fab fa-twitter-square"></i></a>
+                                <a href="#" style={{ fontSize: "50px", marginRight: "10px"}}><i class="fab fa-linkedin"></i></a>
+                                <a href="#" style={{ fontSize: "50px", marginRight: "10px"}}><i class="fab fa-github-square"></i></a>
+                            </div>
                         </div>
+                    </div>
 
+                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", marginTop: "140px", width: "auto" }}> 
+                        Learn more
+                    </div>
+
+                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", marginTop: "50px", width: "auto", height: "300px", backgroundColor: "lightgrey" }}>
+                        Currently living in Redwood City, California.  
                     </div>
             </>
         )
